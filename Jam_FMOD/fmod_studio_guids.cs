a76898a68479f769fcb6896034b5c148e8e7b5d0
Alexey <zlotnikov001@gmail.com>
2023-04-18 22:58:45 +0300

    Snapshot

---
+++ Jam_FMOD/fmod_studio_guids.cs
@@ -0,0 +1,73 @@
+/*
+    fmod_studio_guids.cs - FMOD Studio API
+
+    Generated GUIDs for project 'Jam_FMOD.fspro'
+*/
+
+namespace FSPRO
+{
+    public class Event
+    {
+        public static readonly FMOD.GUID Environment_Achive = new FMOD.GUID { Data1 = 878832541, Data2 = 1197372196, Data3 = -1689428312, Data4 = 2046306041 };
+        public static readonly FMOD.GUID Environment_FirePlace = new FMOD.GUID { Data1 = -1049842371, Data2 = 1219950717, Data3 = -191907413, Data4 = -1420536259 };
+        public static readonly FMOD.GUID Environment_FirePlaceOff = new FMOD.GUID { Data1 = 1796210958, Data2 = 1117129223, Data3 = 537934015, Data4 = 2085360291 };
+        public static readonly FMOD.GUID Environment_Kamish_idle = new FMOD.GUID { Data1 = -356965004, Data2 = 1211637538, Data3 = -971837304, Data4 = -1342621336 };
+        public static readonly FMOD.GUID LocomotionFP_Footsteps_FootstepsFP = new FMOD.GUID { Data1 = -242587917, Data2 = 1080724167, Data3 = -40444239, Data4 = -98226923 };
+        public static readonly FMOD.GUID LocomotionFP_JumpFP_JumpFP = new FMOD.GUID { Data1 = -1145143646, Data2 = 1089467095, Data3 = 1750248101, Data4 = -910780087 };
+        public static readonly FMOD.GUID LocomotionFP_JumpFP_LandFP = new FMOD.GUID { Data1 = -287331561, Data2 = 1140312474, Data3 = 1027811461, Data4 = -2054646113 };
+        public static readonly FMOD.GUID LocomotionFP_Reaction_FP_balloon_inflate_long = new FMOD.GUID { Data1 = -1338513506, Data2 = 1204084552, Data3 = -1633718912, Data4 = -1203077332 };
+        public static readonly FMOD.GUID LocomotionFP_Reaction_FP_deflate = new FMOD.GUID { Data1 = 1768416654, Data2 = 1260760450, Data3 = -705139580, Data4 = 581662191 };
+        public static readonly FMOD.GUID LocomotionFP_Reaction_FP_inflate = new FMOD.GUID { Data1 = -95493117, Data2 = 1141843498, Data3 = 1836363139, Data4 = 362813908 };
+        public static readonly FMOD.GUID LocomotionFP_Reaction_FP_whatDo = new FMOD.GUID { Data1 = 935464587, Data2 = 1322589001, Data3 = -572952703, Data4 = -587406105 };
+        public static readonly FMOD.GUID LocomotionFP_Reaction_FP_whistling = new FMOD.GUID { Data1 = 260662072, Data2 = 1231384440, Data3 = -24278652, Data4 = 638863946 };
+        public static readonly FMOD.GUID Menu_Click = new FMOD.GUID { Data1 = 1330095303, Data2 = 1199691214, Data3 = -488860516, Data4 = -1084406990 };
+        public static readonly FMOD.GUID Menu_Done = new FMOD.GUID { Data1 = 397920337, Data2 = 1160102878, Data3 = 2024833933, Data4 = 917098019 };
+        public static readonly FMOD.GUID Menu_Exit = new FMOD.GUID { Data1 = 609109819, Data2 = 1277959180, Data3 = 796529085, Data4 = -1821149466 };
+        public static readonly FMOD.GUID Menu_Hover = new FMOD.GUID { Data1 = -1207344765, Data2 = 1102980203, Data3 = 2053240201, Data4 = 1692051526 };
+        public static readonly FMOD.GUID Mobs_Mob_whistling = new FMOD.GUID { Data1 = -651574531, Data2 = 1289862329, Data3 = -96365430, Data4 = 754310502 };
+        public static readonly FMOD.GUID Mobs_Spider_in_Kamish = new FMOD.GUID { Data1 = 292579202, Data2 = 1218492380, Data3 = 185662911, Data4 = -1826243812 };
+        public static readonly FMOD.GUID Mobs_VoiceShout = new FMOD.GUID { Data1 = -1666137564, Data2 = 1219741650, Data3 = -1333674586, Data4 = -788348838 };
+        public static readonly FMOD.GUID Music_Music = new FMOD.GUID { Data1 = -1267573975, Data2 = 1192798847, Data3 = 21340299, Data4 = -1709409675 };
+        public static readonly FMOD.GUID New_Event = new FMOD.GUID { Data1 = -621095974, Data2 = 1246247346, Data3 = -956601157, Data4 = -1049152444 };
+        public static readonly FMOD.GUID Scenes_01scene_On_tree_1sc_MusiciansStart = new FMOD.GUID { Data1 = -506699581, Data2 = 1251839051, Data3 = 1983280820, Data4 = -1331356247 };
+        public static readonly FMOD.GUID Scenes_01scene_On_tree_1sc_musiciansStop = new FMOD.GUID { Data1 = 2131314062, Data2 = 1221800568, Data3 = -1127690338, Data4 = -1184730661 };
+        public static readonly FMOD.GUID Scenes_02scene_EyeStrike = new FMOD.GUID { Data1 = 1483546052, Data2 = 1229253550, Data3 = 1817919667, Data4 = -1231190252 };
+        public static readonly FMOD.GUID Scenes_02scene_FallTree = new FMOD.GUID { Data1 = -447860547, Data2 = 1222090781, Data3 = -956527940, Data4 = 1490835374 };
+        public static readonly FMOD.GUID Scenes_02scene_FeatherTrick = new FMOD.GUID { Data1 = 1877801877, Data2 = 1184215325, Data3 = -1638119785, Data4 = 959416510 };
+        public static readonly FMOD.GUID Scenes_02scene_MountainIdle = new FMOD.GUID { Data1 = -1281762320, Data2 = 1086241388, Data3 = -722704466, Data4 = -198864690 };
+        public static readonly FMOD.GUID Scenes_02scene_OpenProp = new FMOD.GUID { Data1 = 566064614, Data2 = 1194411801, Data3 = -2063627879, Data4 = -1057711692 };
+        public static readonly FMOD.GUID Scenes_02scene_RockLaugh = new FMOD.GUID { Data1 = -1646834736, Data2 = 1121853112, Data3 = -224274536, Data4 = -2008918880 };
+        public static readonly FMOD.GUID Scenes_02scene_Sawing = new FMOD.GUID { Data1 = -1769423177, Data2 = 1140953818, Data3 = 383523997, Data4 = 1793439446 };
+        public static readonly FMOD.GUID Test2 = new FMOD.GUID { Data1 = 1981227304, Data2 = 1181693239, Data3 = -1525238393, Data4 = 99958503 };
+        public static readonly FMOD.GUID Traktor_Engine = new FMOD.GUID { Data1 = 471385728, Data2 = 1118797755, Data3 = 1685299385, Data4 = 1581049916 };
+        public static readonly FMOD.GUID Traktor_Tires = new FMOD.GUID { Data1 = -1665193728, Data2 = 1301596808, Data3 = -1751945301, Data4 = -837788326 };
+    }
+
+    public class Bus
+    {
+        public static readonly FMOD.GUID Master_Bus = new FMOD.GUID { Data1 = -1106540604, Data2 = 1152238225, Data3 = -228349031, Data4 = 565176850 };
+        public static readonly FMOD.GUID Ambience = new FMOD.GUID { Data1 = 1203284543, Data2 = 1303167515, Data3 = -830256760, Data4 = -1865582466 };
+        public static readonly FMOD.GUID Ambience_Music = new FMOD.GUID { Data1 = -734082961, Data2 = 1092607832, Data3 = 408187322, Data4 = 1297229324 };
+        public static readonly FMOD.GUID Reverb = new FMOD.GUID { Data1 = -557966218, Data2 = 1187082946, Data3 = 1689863574, Data4 = -1064584821 };
+        public static readonly FMOD.GUID Sounds = new FMOD.GUID { Data1 = 192034161, Data2 = 1259112369, Data3 = -1746882912, Data4 = 2042107474 };
+    }
+
+    public class VCA
+    {
+        public static readonly FMOD.GUID Amb_VCA = new FMOD.GUID { Data1 = -190797538, Data2 = 1084432483, Data3 = -538872652, Data4 = -104614629 };
+        public static readonly FMOD.GUID SFX_VCA = new FMOD.GUID { Data1 = 1237804174, Data2 = 1335086182, Data3 = 167017911, Data4 = -475461980 };
+    }
+
+    public class Snapshot
+    {
+        public static readonly FMOD.GUID PauseSnapshot = new FMOD.GUID { Data1 = 1090219170, Data2 = 1305658369, Data3 = -1723717503, Data4 = -1409644755 };
+    }
+
+    public class Bank
+    {
+        public static readonly FMOD.GUID Bank_A = new FMOD.GUID { Data1 = 1780694317, Data2 = 1185962006, Data3 = -1572642391, Data4 = -1223379922 };
+        public static readonly FMOD.GUID Master = new FMOD.GUID { Data1 = -1671019412, Data2 = 1334206177, Data3 = -1505380963, Data4 = 212262907 };
+    }
+
+}
+
